fdc18fa98189f659dd92f2886f347e727a5f875a
Patrick Schuur <patrickschuurdev@gmail.com>
2024-11-22 16:35:13 +0100

    Added rotating spaceship for my player

--- lib/my_game.dart
+++ lib/my_game.dart
@@ -1,56 +1,23 @@
 import 'package:dutch_game_studio_assessment/player.dart';
 import 'package:flame/game.dart';
 import 'package:flame/events.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flame_forge2d/forge2d_game.dart';
+import 'package:flame_texturepacker/flame_texturepacker.dart';
 
-
-class MyGame extends Forge2DGame with KeyboardEvents {
-  final Player player = Player(Vector2(50, 50));
-  final Map<LogicalKeyboardKey, bool> pressedKeys = {
-    LogicalKeyboardKey.keyW: false,
-    LogicalKeyboardKey.keyA: false,
-    LogicalKeyboardKey.keyS: false,
-    LogicalKeyboardKey.keyD: false,
-  };
+class MyGame extends Forge2DGame with HasKeyboardHandlerComponents{
+  late Player player;
 
   @override
   Future<void> onLoad() async {
+
+    final atlas = await atlasFromAssets('HellInSpaceTextures.atlas');
+
+    final playerSprite = atlas.findSpriteByName('player');
+    assert(playerSprite != null);
+    player = Player(Vector2(50, 50), playerSprite);
+
+
+    world.gravity = Vector2.zero();
     add(player);
-  }
-
-  @override
-  KeyEventResult onKeyEvent(
-      KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
-    
-    for (LogicalKeyboardKey key in pressedKeys.keys){
-      pressedKeys[key] = keysPressed.contains(key);
-    }
-
-    return super.onKeyEvent(event, keysPressed);
-  }
-
-  @override void update(double dt) {
-    Vector2 direction = Vector2.zero();
-
-    if (pressedKeys[LogicalKeyboardKey.keyW]!) {
-      direction.y -= 1;
-    }
-
-    if (pressedKeys[LogicalKeyboardKey.keyS]!) {
-      direction.y += 1;
-    }
-
-    if (pressedKeys[LogicalKeyboardKey.keyA]!){
-      direction.x -= 1;
-    }
-
-    if (pressedKeys[LogicalKeyboardKey.keyD]!){
-      direction.x += 1;
-    }
-
-    player.translate(direction * 1000 * dt);
-    super.update(dt);
   }
 }
--- lib/player.dart
+++ lib/player.dart
@@ -1,40 +1,91 @@
+import 'dart:math';
+
 import 'package:flame/components.dart';
+import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flutter/services.dart';
 
-class Player extends RectangleComponent with KeyboardHandler {
-  Player(Vector2 position)
-      : super(
-          position: position,
-          size: Vector2.all(128),
-          anchor: Anchor.center,
-        );
+class Player extends BodyComponent with KeyboardHandler {
+  final Map<LogicalKeyboardKey, bool> _pressedKeys = {
+    LogicalKeyboardKey.keyW: false,
+    LogicalKeyboardKey.keyA: false,
+    LogicalKeyboardKey.keyS: false,
+    LogicalKeyboardKey.keyD: false,
+  };
 
-  void translate(Vector2 translation){
-    position += translation;
+  final BodyType _bodyType = BodyType.dynamic;
+  final Vector2 _position;
+  final double _radius = 10;
+  final double _impulseForce = 10000;
+
+  Player(this._position, sprite)
+      : super(renderBody: false, children: [
+          _PlayerSpriteComponent(sprite),
+        ]);
+
+  @override
+  Body createBody() {
+    final shape = CircleShape();
+    shape.radius = _radius;
+
+    final fixtureDef = FixtureDef(
+      shape,
+      friction: 1.0,
+    );
+    final bodyDef = BodyDef(
+      position: _position,
+      type: _bodyType,
+      linearDamping: 0.6,
+    );
+
+    final massData = MassData();
+    massData.mass = 10;
+
+    return world.createBody(bodyDef)
+      ..createFixture(fixtureDef)
+      ..setMassData(massData);
   }
 
   @override
   bool onKeyEvent(KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
-    Vector2 direction = Vector2.zero();
-
-    if (keysPressed.contains(LogicalKeyboardKey.keyW)) {
-      direction.y -= 1;
+    for (LogicalKeyboardKey key in _pressedKeys.keys) {
+      _pressedKeys[key] = keysPressed.contains(key);
     }
-
-    if (keysPressed.contains(LogicalKeyboardKey.keyS)){
-      direction.y += 1;
-    }
-
-    if (keysPressed.contains(LogicalKeyboardKey.keyA)){
-      direction.x -= 1;
-    }
-
-    if (keysPressed.contains(LogicalKeyboardKey.keyD)){
-      direction.x += 1;
-    }
-
-    position += direction * 10;
 
     return super.onKeyEvent(event, keysPressed);
   }
+
+  @override
+  void update(double dt) {
+    Vector2 direction = Vector2.zero();
+
+    if (_pressedKeys[LogicalKeyboardKey.keyW]!) {
+      direction.y -= 1;
+    }
+
+    if (_pressedKeys[LogicalKeyboardKey.keyS]!) {
+      direction.y += 1;
+    }
+
+    if (_pressedKeys[LogicalKeyboardKey.keyA]!) {
+      direction.x -= 1;
+    }
+
+    if (_pressedKeys[LogicalKeyboardKey.keyD]!) {
+      direction.x += 1;
+    }
+
+    body.applyLinearImpulse(direction * _impulseForce * dt);
+
+    double targetAngle = atan2(body.linearVelocity.y, body.linearVelocity.x) +
+        (0.5 * pi);
+
+    body.setTransform(body.position, targetAngle);
+    body.angularVelocity = 0;
+
+    super.update(dt);
+  }
+}
+
+class _PlayerSpriteComponent extends SpriteComponent {
+  _PlayerSpriteComponent(sprite) : super(anchor: Anchor.center, sprite: sprite);
 }
